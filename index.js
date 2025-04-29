c0923577ecb64c4c5ed71c8632cfd9898e2c69dd
Pera Peric <pera.peric@gmail.com>
2025-04-29 15:29:04 +0200

    Formirana tabela spram podataka u nizu, crvenom bojom obojeni redovi koji nisu na stanju

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 'use strict'
 
+function kreirajRed(artikal){
+    
+    let tr = document.createElement("tr")
+
+    let brojArtikla = document.createElement("td")
+    let naziv = document.createElement("td")
+    let cena = document.createElement("td")
+    let dostupan = document.createElement("td")
+
+    brojArtikla.style.border = '1px solid black'
+    naziv.style.border = '1px solid black'
+    cena.style.border = '1px solid black'
+    dostupan.style.border = '1px solid black'
+
+
+    brojArtikla.textContent = artikal.brojArtikla
+    naziv.textContent = artikal.naziv
+    cena.textContent= artikal.cena + "e"
+    dostupan.textContent= artikal.dostupan ? "DA" : "NE"
+
+
+    if(!artikal.dostupan){
+        tr.style.backgroundColor = "#f8cccc"
+    }
+    
+    tr.appendChild(brojArtikla)
+    tr.appendChild(naziv)
+    tr.appendChild(cena)
+    tr.appendChild(dostupan)
+
+    return tr
+}
+
+function inicijalizujArtikle(artikal){
+    let table = document.querySelector("#artikli")
+    table.style.borderCollapse = "collapse";
+    for(let artikal of artikli){
+        let tr= kreirajRed(artikal)
+        table.appendChild(tr)
+    }
+
+}
+
 class Artikal{
     constructor(brojArtikla, naziv, cena, dostupan){
         this.brojArtikla= brojArtikla
@@ -15,9 +58,4 @@ const treciArtikal = new Artikal(3, "Mis", 20, true)
 
 const artikli = [prviArtikal, drugiArtikal, treciArtikal]
 
-let table = document.querySelector("#artikli")
-
-for (let artikal of artikli){
-    let tr = document.createElement("tr")
-    tr.style.border = '1px solid black'
-}
+inicijalizujArtikle(artikli)
